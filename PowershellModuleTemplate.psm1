47128cec0cee144e942f5f23b2f29586619ca04d
Xinyun Yang <yangxinyun204@gmail.com>
2021-10-29 12:53:31 +0200

    added 1st version for template files

---
+++ PowershellModuleTemplate.psm1
@@ -0,0 +1,50 @@
+<#
+.SYNOPSIS
+  <Overview of script>
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER <Parameter_Name>
+  <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None>
+.NOTES
+  Version:        0.0.0
+  Author:         YourName
+  Creation Date:  01/04/2020
+  Purpose/Change: Initial script development
+.EXAMPLE
+  <Example explanation goes here>
+  
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+Function <FunctionName> {
+  Param ()
+  Begin {
+    Write-Host '<description of what is going on>...'
+  }
+  Process {
+    Try {
+      <code goes here>
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed Successfully.'
+      Write-Host ' '
+    }
+  }
+}
+#>
+
+#-----------------------------------------------------------[Export Functions]------------------------------------------------------------
+
+#Export-ModuleMember -Function <FunctionName>
---
+++ PowershellTemplate.ps1
@@ -0,0 +1,74 @@
+#requires -version 5.1
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  <Overview of script>
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER <Parameter_Name>
+  <Brief description of parameter input required. Repeat this attribute if required>
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None>
+.NOTES
+  Version:        0.0.0
+  Author:         YourName
+  Creation Date:  01/04/2020
+  Purpose/Change: Initial script development
+.EXAMPLE
+  <Example explanation goes here>
+  
+  <Example goes here. Repeat this attribute for more than one example>
+#>
+
+#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+
+Param (
+  #Script parameters go here
+)
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+$DebugPreference = 'Continue'
+$VerbosePreference = 'Continue'
+$InformationPreference = 'Continue'
+$WarningPreference = 'Continue'
+$ErrorActionPreference = 'Stop'
+
+#Import Modules & Snap-ins
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Any Global Declarations go here
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+<#
+Function <FunctionName> {
+  Param ()
+  Begin {
+    Write-Host '<description of what is going on>...'
+  }
+  Process {
+    Try {
+      <code goes here>
+    }
+    Catch {
+      Write-Host -BackgroundColor Red "Error: $($_.Exception)"
+      Break
+    }
+  }
+  End {
+    If ($?) {
+      Write-Host 'Completed Successfully.'
+      Write-Host ' '
+    }
+  }
+}
+#>
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+#Script Execution goes here
